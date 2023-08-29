4c4252d759c7bbda87d369c7703f401ee23a6460
bioha33ard <wolfq7522@gmail.com>
2023-08-29 20:35:50 +0300

    add Telegram Bot Api

--- app/Http/Controllers/API/OrdersController.php
+++ app/Http/Controllers/API/OrdersController.php
@@ -9,14 +9,20 @@ use App\Models\Order;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Telegram\Bot\Laravel\Facades\Telegram;
 use Throwable;
 
 class OrdersController extends Controller
 {
+    protected function getChatId() : array
+    {
+     return explode(',', ENV('TELEGRAM_CHAT_ID'));
+    }
+
     /**
      * Display a listing of the resource.
      *
-     * @return Response
+     * @return \Telegram\Bot\Objects\Message
      */
     public function index()
     {
@@ -33,6 +39,31 @@ class OrdersController extends Controller
     {
         try {
             $order = Order::create($request->all());
+            foreach ($this->getChatId() as $chat_id) {
+                Telegram::sendMessage([
+                    'chat_id' => $chat_id,
+                    'text' => 'Новый заказ № '. $order->id,
+                ]);
+                Telegram::sendMessage([
+                    'chat_id' => $chat_id,
+                    'text' => "
+ФИО : {$request->input('full_name')}
+Контакты : {$request->input('contacts')}
+Модель авто. : {$request->input('model')}
+Марка авто. : {$request->input('brand')}
+Год выпуска. : {$request->input('year')}
+Тип двигателя : {$request->input('engine_type')}
+Обьем двигателя :{$request->input('engine_volume')}
+КПП : {$request->input('transmission')}
+Привод : {$request->input('drive')}
+Мощность двигателя: {$request->input('horse_power')}
+Кузов : {$request->input('car_body')}
+Руль : {$request->input('wheel')}
+Качество : {$request->input('quality')}
+                                ",
+                ]);
+            }
+
             return new OrderResource($order);
         } catch (\Exception $exception) {
             return response()
